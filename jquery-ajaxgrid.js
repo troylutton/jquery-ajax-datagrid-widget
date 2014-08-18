f43becf2f1b3922f8953976935ed40e77df2f822
troylutton <troy.lutton@gmail.com>
2014-08-19 09:07:37 +1000

    Added events hook for when content is refreshed

--- jquery-ajaxgrid.js
+++ jquery-ajaxgrid.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Generic datagrid Widget to wrap common functions such as:
  *
  *      - ajax grid paging
@@ -30,6 +30,9 @@
 
         options: {
 
+            // optional for remote grid location refreshing
+            url: undefined,
+
             // grid config
             dataGridClass: ".data-grid",
             containerClass: ".data-grid-container",
@@ -41,6 +44,9 @@
             // pagination config
             paginationParamName: 'page',
             paginationClass: ".grid-pagination",
+
+            // add custom events to the grid once build and each time its refreshed
+            events: function(element, seachFormId) {},
 
             // grid search functionality
             search: {
@@ -87,102 +93,115 @@
                 self._doPagination(this, self.element);
             });
 
+            // get the scoped serch form selector. must support event bubbling
+            var searchFormId = this.element.data(this.options.searchDataAttribute);
+
             // see if this datagrid has a seach form
-            if (this.element.data(this.options.searchDataAttribute)) {
+            if ($(searchFormId).length > 0) {
 
-                // hijack the form submission
-                var searchForm = $(this.element.data(this.options.searchDataAttribute));
+                // HIJACK THE RESET
 
-                if (searchForm.length > 0) {
+                // hijack the sorting links with ajax calls; delegated version for bubbled events
+                $(document).on("click", searchFormId + ' .grid-search-reset', function (e) {
+                    e.preventDefault();
+                    // clear the search and reload the default grid using the reset url
+                    $('input', searchFormId).val('');
+                    self._getGridAndPagination(this.href, {}, self.element);
+                });
 
-                    // HIJACK SEARCH SUBMIT
+                // HIJACK SEARCH SUBMIT
 
-                    // look for a hijack method for submitting results
-                    if (!this.options.search.submit) {
+                // look for a hijack method for submitting results
+                if (!this.options.search.submit) {
 
-                        // use the default submitter
-                        this.options.search.submit = function (gridContainer, searchContainer) {
-                            self._getSearchResults(gridContainer, searchContainer);
-                        }
-
-                        // attach the submit event
-                        searchForm.submit(function (e) {
-                            e.preventDefault();
-                            self.options.search.submit(this.element, searchForm);
-                        });
-                    }
-                    
-                    // ATTACH AUTOCOMPLETE
-
-                    // attach an autocomplete where required
-                    if (true == this.options.search.autocomplete.enabled) {
-
-                        if (!this.options.search.autocomplete.worker) {
-                            // use the default autocomplete
-                            this.options.search.autocomplete.worker = function (gridContainer, form, opt) {
-                                self._attachAutocomplete(gridContainer, form, opt);
-                            }
-                        }
-
-                        if (!this.options.search.autocomplete.renderer) {
-                            // use the default renderer; convention based
-                            this.options.search.autocomplete.renderer = function (ul, item) {
-                                return self._renderAutocompleteItem(ul, item);
-                            }
-                        }
-                        
-                        // build some options
-                        var options = {
-                            url: null == this.options.search.autocomplete.url ? window.location.pathname + '/search' : this.options.search.autocomplete.url
-                        };
-
-                        // call the worker to attach the autocomplete
-                        this.options.search.autocomplete.worker(this.element, searchForm, options);
-                    }
-
-                    // FILTER RESULTS AS SEARCH IS DONE
-
-                    if (true == this.options.search.filterResults) {
-
-                        //setup before functions
-                        var typingTimer;                //timer identifier
-                        var doneTypingInterval = 500;  //time in ms, 1/2 second for example
-
-                        // filter the viewable results as we go
-                        $(this.options.searchFieldClass, searchForm).keyup(function (e) {
-
-                            /*if (e.which.code == 13) {
-                                return clearTimeout(typingTimer);
-                            }*/
-
-                            clearTimeout(typingTimer);
-                            typingTimer = setTimeout(function() {
-                                self.options.search.submit(self.element, searchForm);    
-                            }, doneTypingInterval);
-                        });
-
-                        //on keydown, clear the countdown 
-                        $(this.options.searchFieldClass, searchForm).keydown(function () {
-                            clearTimeout(typingTimer);
-                        });
+                    // use the default submitter
+                    this.options.search.submit = function (gridContainer, formId) {
+                        self._getSearchResults(gridContainer, formId);
                     }
                 }
+
+                // attach the submit event
+                $(document).on("submit", searchFormId + ' form', function (e) {
+                    e.preventDefault();
+                    self.options.search.submit(this.element, searchFormId);
+                });
+                    
+                // ATTACH AUTOCOMPLETE
+
+                // attach an autocomplete where required
+                if (true == this.options.search.autocomplete.enabled) {
+
+                    if (!this.options.search.autocomplete.worker) {
+                        // use the default autocomplete
+                        this.options.search.autocomplete.worker = function (gridContainer, form, opt) {
+                            self._attachAutocomplete(gridContainer, form, opt);
+                        }
+                    }
+
+                    if (!this.options.search.autocomplete.renderer) {
+                        // use the default renderer; convention based
+                        this.options.search.autocomplete.renderer = function (ul, item) {
+                            return self._renderAutocompleteItem(ul, item);
+                        }
+                    }
+                        
+                    // build some options
+                    var options = {
+                        url: null == this.options.search.autocomplete.url ? window.location.pathname + '/search' : this.options.search.autocomplete.url
+                    };
+
+                    // call the worker to attach the autocomplete
+                    this.options.search.autocomplete.worker(this.element, searchFormId, options);
+                }
+
+                // FILTER RESULTS AS SEARCH IS DONE
+
+                if (true == this.options.search.filterResults) {
+
+                    //setup before functions
+                    var typingTimer;                //timer identifier
+                    var doneTypingInterval = 500;  //time in ms, 1/2 second for example
+
+                    // filter the viewable results as we go
+                    $(document).on("keyup", searchFormId + ' ' + this.options.searchFieldClass, function (e) {
+                        /*if (e.which.code == 13) {
+                            return clearTimeout(typingTimer);
+                        }*/
+
+                        clearTimeout(typingTimer);
+                        typingTimer = setTimeout(function() {
+                            self.options.search.submit(self.element, searchFormId);    
+                        }, doneTypingInterval);
+                    });
+
+                    //on keydown, clear the countdown 
+                    $(document).on("keydown", searchFormId + ' ' + this.options.searchFieldClass, function (e) {
+                        clearTimeout(typingTimer);
+                    });
+                }
             }
+
+            // add custom events
+            this.options.events(this.element, searchFormId);
 
             // hook_init()
             this._trigger('afterInit', null, this);
         },
 
-        _getSearchResults: function (gridContainer, searchForm) {
-
+        _getSearchResults: function (gridContainer, searchFormId) {
+            var searchForm = $(searchFormId);
+            if (!searchForm.is('form')) {
+                searchForm = $('form', searchForm);
+            }
             // query the server and replace the contents of the grid
             this._getGridAndPagination(searchForm.attr('action'), searchForm.serialize(), gridContainer, 'afterSearch', 'post');
         },
 
-        _attachAutocomplete: function (gridContainer, searchForm, options) {
+        _attachAutocomplete: function (gridContainer, searchFormId, options) {
 
             // init
             var self = this;
+            var searchForm = $('form', searchFormId);
             var searchBox = $(this.options.searchFieldClass, searchForm);
             var url = null == options.url ? searchForm.attr('action') : options.url;
 
@@ -260,11 +279,28 @@
                 grid.html(result.find(self.options.dataGridClass).html());
                 // replace pagination
                 pagination.html(result.find(self.options.paginationClass).html());
+
+                // re-add custom events
+                self.options.events(self.element, self.element.data(self.options.searchDataAttribute));
+
                 // hook_x() expects string callback name
                 if (callback != undefined) {
-                    self._trigger(callback, null, this);
+                    // direct closure callback or hook
+                    if (typeof callback == 'function') {
+                        callback.call();
+                    }
+                    else
+                    {
+                        // call hook_x
+                        self._trigger(callback, null, this);
+                    }
                 }
             });
+        },
+
+        // provide a refresh capability
+        refreshGrid: function (callback) {
+            this._getGridAndPagination(this.options.url, {}, this.element, callback);
         },
 
         // mandatory function
